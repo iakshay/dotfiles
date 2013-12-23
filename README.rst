91330e13e950cf3971e4c64833c6c0e4c7312989
Akshay Aurora <akshayaurora@yahoo.com>
2013-12-24 02:28:55 +0530

    Adding custom bash alias and functions

--- README.rst
+++ README.rst
@@ -3,4 +3,6 @@ Akshay's dotfiles
 
 Switch to oh-my-zsh for awesomeness!
 
-**TODO** - Create bash script to create symlinks
+**TODO** 
+- Create bash script to create symlinks
+- Add weechat stuff; irc.conf, weechat.conf, scripts?
--- oh-my-zsh/custom/example.zsh
+++ oh-my-zsh/custom/example.zsh
@@ -1,5 +1,43 @@
-# Add yourself some shortcuts to projects you often work on
-# Example:
-#
-# brainstormr=/Users/robbyrussell/Projects/development/planetargon/brainstormr
-#
+# functions to switch hosts files, to avoid distractions
+function isolate(){
+	ISOLATION_MODE=true
+	cp /etc/hosts.blocked /etc/hosts
+}
+
+function unisolate(){
+	ISOLATION_MODE=false
+	cp /etc/hosts.unblocked /etc/hosts
+}
+
+function isolate_prompt_info(){
+	if [[ "$ISOLATION_MODE" == "true" ]]
+	then
+		echo "\e[38;5;196m◕︵◕"
+	else
+		echo -e "\e[38;5;190m◕◡◕"
+	fi
+}
+
+#reload zshrc
+alias reload='source ~/.zshrc'
+
+# Start an HTTP server from a directory, optionally specifying the port
+function server() {
+	local port="${1:-8000}"
+	open "http://localhost:${port}/"
+	# Set the default Content-Type to `text/plain` instead of `application/octet-stream`
+	# And serve everything as UTF-8 (although not technically correct, this doesn’t break anything for binary files)
+	python -c $'import SimpleHTTPServer;\nmap = SimpleHTTPServer.SimpleHTTPRequestHandler.extensions_map;\nmap[""] = "text/plain";\nfor key, value in map.items():\n\tmap[key] = value + ";charset=UTF-8";\nSimpleHTTPServer.test();' "$port"
+}
+
+# Create a new directory and enter it
+function md() {
+	mkdir -p "$@" && cd "$@"
+}
+
+#Todo - Show time since last commit in prompt
+function git_time_since_commit(){
+  #echo $(git log -1 --pretty=format:"%ad" --date="relative")
+}
+
+export isolate_prompt_info
