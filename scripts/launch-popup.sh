e584d3504a83d5d916cc2cb928beff8ed4938fa0
Akshay Aurora <akshayaurora@yahoo.com>
2024-11-16 17:51:09 -0800

    update tmux scripts

--- scripts/launch-popup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# https://blog.meain.io/2020/tmux-flating-scratch-terminal/
-if [ "$(tmux display-message -p -F "#{session_name}")" = "popup" ];then
-    tmux detach-client
-else
-    tmux popup -E $HOME/.dotfiles/scripts/show-tmux-popup.sh
-fi
--- scripts/show-tmux-popup.sh
+++ scripts/show-tmux-popup.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
+export PATH=$PATH:/opt/homebrew/bin
 # https://willhbr.net/2023/02/07/dismissable-popup-shell-in-tmux/
 session="popup"
 
 if ! tmux has -t "$session" 2> /dev/null; then
   session_id="$(tmux new-session -dP -s "$session" -F '#{session_id}')"
-#   tmux set-option -s -t "$session_id" key-table popup
+  tmux set-option -s -t "$session_id" key-table popup
   tmux set-option -s -t "$session_id" status off
-#   tmux set-option -s -t "$session_id" prefix None
+  tmux set-option -s -t "$session_id" prefix None
   session="$session_id"
 fi
 
